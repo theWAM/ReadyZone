02cb501f80fa68bcbd470f71e439b96eddb673f9
the Woody Allen M <woodablu@gmail.com>
2019-03-27 11:15:20 -0400

    rewrite library imports

--- startup.py
+++ startup.py
@@ -1,4 +1,5 @@
 from win32gui import GetWindowText, GetForegroundWindow, SetWindowPos, GetWindowRect
+import win32gui as wg
 import win32con
 import os, time
 import pyautogui as pg
@@ -19,6 +20,7 @@ def set_window_top():
     hwnd = GetForegroundWindow()
     (left, top, right, bottom) = GetWindowRect(hwnd)
     SetWindowPos(hwnd, win32con.HWND_TOPMOST, left, top, (right - left)+250, bottom - top, 0) 
+    
 
 pg.hotkey('shift', 'alt', 'tab')
 time.sleep(2)
